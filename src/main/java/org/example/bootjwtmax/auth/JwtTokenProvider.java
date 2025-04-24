11881df9634b54d109e613bff81b12f869bb495f
JO KYEONGHYE <shienka07.ko@gmail.com>
2025-04-24 11:41:08 +0900

    JwtTokenProvider

---
+++ src/main/java/org/example/bootjwtmax/auth/JwtTokenProvider.java
@@ -0,0 +1,66 @@
+package org.example.bootjwtmax.auth;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.time.Instant;
+import java.util.Date;
+import java.util.List;
+
+@Component
+public class JwtTokenProvider {
+    @Value("${jwt.secret}")
+    private String secretKey;
+    @Value("@{jwt.expiration-ms}")
+    private long expirationMs;
+
+    public SecretKey getSecretKey() {
+        return Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
+
+    public String generateToken(Authentication authentication) {
+        String username = authentication.getName();
+        Instant now = Instant.now();
+        Date expiration = new Date(now.toEpochMilli() + expirationMs);
+        return Jwts.builder()
+                .subject(username) // uuid 를 넣어도 됨
+                .issuedAt(Date.from(now)) // 토큰 생성 일자
+                .expiration(expiration) // 토큰 만료 일자
+                .signWith(getSecretKey(), Jwts.SIG.HS256) // 변환 알고리즘
+                .compact();
+    }
+
+    public String getUsername(String token) {
+        return Jwts.parser()
+                .verifyWith(getSecretKey())
+                .build()
+                .parseSignedClaims(token)// // 1. 양식을 지켰나? 2. 키와 대응 3. 만료되었는가?
+                .getPayload() // 유저 데이터
+                .getSubject(); // username
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser()
+                    .verifyWith(getSecretKey())
+                    .build()
+                    .parseSignedClaims(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    public Authentication getAuthentication(String token) {
+        UserDetails user = new User(getUsername(token), "", List.of());
+        return new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+    }
+}
