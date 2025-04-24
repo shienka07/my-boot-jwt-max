ffc0aff39c952f85e69cce010792d2cc11b8a714
JO KYEONGHYE <shienka07.ko@gmail.com>
2025-04-24 11:03:59 +0900

    SecurityConfig

--- src/main/java/org/example/bootjwtmax/auth/SecurityConfig.java
+++ src/main/java/org/example/bootjwtmax/auth/SecurityConfig.java
@@ -1,4 +1,19 @@
 package org.example.bootjwtmax.auth;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // 전체 허용
+        http.authorizeHttpRequests(
+                auth -> auth.anyRequest().permitAll()
+        );
+        return http.build();
+    }
 }
